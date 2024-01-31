f63a962a01a27443def27dbeb6d71cc61d2ac261
GhizlaneMeq <meqdarghizlane@gmail.com>
2024-01-31 10:22:32 +0100

    la creation du migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('phone');
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+             $table->timestamp('email_verified_at')->nullable();
+             $table->rememberToken();
             $table->timestamps();
         });
+
     }
 
     /**
---
+++ database/migrations/2024_01_30_220156_create_emprunts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('emprunts', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->date('date_emprunt');
+            $table->date('return_date')->nullable();
+            $table->date('is_returned')->nullable(); 
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('book_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('emprunts');
+    }
+};
