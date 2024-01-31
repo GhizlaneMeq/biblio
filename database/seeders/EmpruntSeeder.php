973ca38b477b53921f978d4eff65471864fa5001
GhizlaneMeq <meqdarghizlane@gmail.com>
2024-01-31 10:26:06 +0100

    la creation des seeders

---
+++ database/seeders/EmpruntSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Emprunt;
+
+use Illuminate\Database\Seeder;
+class EmpruntSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Emprunt::factory()->count(10)->create();
+    }
+}
